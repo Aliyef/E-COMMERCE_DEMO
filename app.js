980eb84c88c946dfe0a3571e698420643e55ca73
Aliyef <80645715+Aliyef@users.noreply.github.com>
2021-07-20 22:30:41 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+$('.home-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+     
+        100:{
+            items:1
+        },
+        400:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
